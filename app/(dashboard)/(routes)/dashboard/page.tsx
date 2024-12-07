43369b7b310e57f8fdc5f88b81504f7fa815d8d6
Aurelle Wafo <59320937+AuriStark@users.noreply.github.com>
2024-12-07 13:26:47 +0100

    Created routes structure and  main pages

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+const DashboardPage = () =>  {
+  return (    
+    <div>
+        <p>
+            This is the Dashboard Page
+        </p>
+    </div>
+    );
+}
+
+export default DashboardPage;
+  
